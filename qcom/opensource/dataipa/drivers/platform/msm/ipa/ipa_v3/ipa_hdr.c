547ac515cb56be05ad8cce79f089bec39cb5569f
Shiva Ganesh <quic_shivgane@quicinc.com>
2024-10-16 17:24:07 +0530

    ipa: Added changes to move the hdr entry to free list
    Added changes to move the hdr free offset list after deteting the
    hdrs from the hdr table if delted hdr is pointing to the some
    proc ctx.
    
    Change-Id: Ic1f20fc8070ae7a45c2d7a5abd5b9c56a96c49cf
    Signed-off-by: Avinash Kumar <quic_avku@quicinc.com>
    Signed-off-by: Shiva Ganesh <quic_shivgane@quicinc.com>

--- qcom/opensource/dataipa/drivers/platform/msm/ipa/ipa_v3/ipa_hdr.c
+++ qcom/opensource/dataipa/drivers/platform/msm/ipa/ipa_v3/ipa_hdr.c
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
  * Copyright (c) 2012-2021, The Linux Foundation. All rights reserved.
- * Copyright (c) 2023-2025 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
  */
 
 #include "ipa_i.h"
@@ -915,8 +915,10 @@ static int __ipa3_del_hdr_proc_ctx(u32 proc_ctx_hdl,
 			proc_ctx_hdl, entry->ref_cnt);
 		return 0;
 	}
+	if (entry->hdr && (entry == entry->hdr->proc_ctx))
+		entry->hdr->proc_ctx = NULL;
 
-	if (release_hdr)
+	if (entry->hdr && release_hdr)
 		__ipa3_del_hdr(entry->hdr->id, false);
 
 	/* move the offset entry to appropriate free list */
@@ -1006,12 +1008,16 @@ int __ipa3_del_hdr(u32 hdr_hdl, bool by_user)
 		return 0;
 	}
 
+	if (entry->proc_ctx && (entry == entry->proc_ctx->hdr))
+		entry->proc_ctx->hdr = NULL;
+
 	if (entry->proc_ctx)
 		__ipa3_del_hdr_proc_ctx(entry->proc_ctx->id, false, false);
-	else
-		/* move the offset entry to appropriate free list */
-		list_move(&entry->offset_entry->link,
-			&htbl->head_free_offset_list[entry->offset_entry->bin]);
+
+	/* move the offset entry to appropriate free list */
+	list_move(&entry->offset_entry->link,
+		&htbl->head_free_offset_list[entry->offset_entry->bin]);
+
 	list_del(&entry->link);
 	htbl->hdr_cnt--;
 	entry->cookie = 0;
