641a1d570d65d1454401b6a995e91b2903709ded
Venkatasaiee12 <dvenkatasai2025@gmail.com>
2026-06-13 15:57:56 +0530

    treewide: Reduce logspam

--- qcom/opensource/wlan/qcacld-3.0/core/hdd/src/wlan_hdd_main.c
+++ qcom/opensource/wlan/qcacld-3.0/core/hdd/src/wlan_hdd_main.c
@@ -19325,7 +19325,7 @@ static int  wlan_hdd_state_ctrl_param_create(void)
 		goto cdev_add_err;
 	}
 
-	pr_info("wlan_hdd_state %s major(%d) initialized",
+	pr_debug_once("wlan_hdd_state %s major(%d) initialized",
 		WLAN_CTRL_NAME, wlan_hdd_state_major);
 
 	return 0;
@@ -19367,7 +19367,7 @@ static void wlan_hdd_state_ctrl_param_destroy(void)
 	class_destroy(class);
 	unregister_chrdev_region(device, dev_num);
 
-	pr_info("Device node unregistered");
+	pr_debug_once("Device node unregistered");
 }
 
 #else /* WLAN_CTRL_NAME */
@@ -20534,10 +20534,10 @@ void hdd_driver_unload(void)
 
 	soft_unload = hdd_get_wlan_driver_status();
 	if (soft_unload) {
-		pr_info("%s: Soft Unloading driver v%s\n", WLAN_MODULE_NAME,
+		pr_debug_once("%s: Soft Unloading driver v%s\n", WLAN_MODULE_NAME,
 			QWLAN_VERSIONSTR);
 	} else {
-		pr_info("%s: Hard Unloading driver v%s\n", WLAN_MODULE_NAME,
+		pr_debug_once("%s: Hard Unloading driver v%s\n", WLAN_MODULE_NAME,
 			QWLAN_VERSIONSTR);
 	}
 
