CodeLinaro-mirror/la_platform_vendor_opensource_camera-kernel

msm: camera: common: Fix buffer validation check
Fix buffer validation check to account for command
buffer descriptor size.

CRs-Fixed: 4293509
Change-Id: I2c8d9e421c6854a2f4ae496bc823d222f43a18ed
Signed-off-by: Dharmender Sharma <dharshar@qti.qualcomm.com>
(cherry picked from commit 888d1af08e4c9248aacc56b9a711245c60066320)

drivers/cam_utils/cam_packet_util.c
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
  * Copyright (c) 2017-2021, The Linux Foundation. All rights reserved.
- * Copyright (c) 2022-2025, Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
  */
 
 #include <linux/types.h>
@@ -181,9 +181,13 @@ int cam_packet_util_get_kmd_buffer(struct cam_packet *packet,
 	}
 
 	remain_len -= (size_t)cmd_desc->offset;
-	if ((size_t)packet->kmd_cmd_buf_offset >= remain_len) {
-		CAM_ERR(CAM_UTIL, "Invalid kmd cmd buf offset: %zu",
-			(size_t)packet->kmd_cmd_buf_offset);
+	if ((size_t)packet->kmd_cmd_buf_offset >= remain_len ||
+		(size_t)(cmd_desc->size - cmd_desc->length) >=
+		(remain_len - packet->kmd_cmd_buf_offset)) {
+		CAM_ERR(CAM_UTIL,
+			"Invalid kmd cmd buf offset: %d remain_len: %d cmd offset: %d size: %d length: %d",
+			packet->kmd_cmd_buf_offset, remain_len,
+			cmd_desc->offset, cmd_desc->size, cmd_desc->length);
 		rc = -EINVAL;
 		goto rel_kmd_buf;
 	}

sai-g3p/e

qcom/opensource/camera-kernel/drivers/cam_utils/cam_packet_util.c
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
  * Copyright (c) 2017-2021, The Linux Foundation. All rights reserved.
- * Copyright (c) 2022-2025, Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
  */
 
 #include <linux/types.h>
@@ -223,9 +223,14 @@ int cam_packet_util_get_kmd_buffer(struct cam_packet *packet,
 	}
 
 	remain_len -= (size_t)cmd_desc->offset;
-	if ((size_t)packet->kmd_cmd_buf_offset >= remain_len) {
-		CAM_ERR(CAM_UTIL, "Invalid kmd cmd buf offset: %zu",
-			(size_t)packet->kmd_cmd_buf_offset);
+	if ((size_t)packet->kmd_cmd_buf_offset >= remain_len ||
+		(size_t)packet->kmd_cmd_buf_offset >= cmd_desc->size ||
+		(size_t)(cmd_desc->size - packet->kmd_cmd_buf_offset) >=
+		(remain_len - packet->kmd_cmd_buf_offset)) {
+		CAM_ERR(CAM_UTIL,
+			"Invalid kmd cmd buf offset: %d remain_len: %zu cmd offset: %d size: %d length: %d",
+			packet->kmd_cmd_buf_offset, remain_len,
+			cmd_desc->offset, cmd_desc->size, cmd_desc->length);
 		rc = -EINVAL;
 		goto rel_kmd_buf;
 	}
@@ -241,7 +246,7 @@ int cam_packet_util_get_kmd_buffer(struct cam_packet *packet,
 	kmd_buf->cpu_addr   = cpu_addr;
 	kmd_buf->handle     = cmd_desc->mem_handle;
 	kmd_buf->offset     = cmd_desc->offset + packet->kmd_cmd_buf_offset;
-	kmd_buf->size       = cmd_desc->size - cmd_desc->length;
+	kmd_buf->size       = cmd_desc->size - packet->kmd_cmd_buf_offset;
 	kmd_buf->used_bytes = 0;
 	return rc;
 